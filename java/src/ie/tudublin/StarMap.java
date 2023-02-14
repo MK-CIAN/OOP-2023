753452a515ef2dc19d402a464ba08ca878404c2c
Cian Mckenna <cianmckenna@Cians-MacBook-Air.local>
2023-02-14 10:43:12 +0000

    Grid Complete

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -6,7 +6,7 @@ public class StarMap extends PApplet
 {
 	public void settings()
 	{
-		size(500, 500);
+		size(800, 800);
 	}
 
 	public void setup() {
@@ -15,11 +15,29 @@ public class StarMap extends PApplet
 
 		smooth();
 		
+
 	}
 
-		
+	public void drawGrid()
+	{
+		stroke(255);
+		float border = 50.0f;
+
+		int count = 10;
+		float gap = (width - (border * 2.0f)) / (float) count;
+
+		for (int i = -5; i <= 5; i++) 
+		{
+			float x = border + (gap * (i + 5));
+			line(x, border, x, height - border);
+			line(border, x, width - border, x);
+		}
+	}
+
+
 	public void draw()
 	{	
-		strokeWeight(2);		
+		strokeWeight(2);	
+		drawGrid();	
 	}
 }
