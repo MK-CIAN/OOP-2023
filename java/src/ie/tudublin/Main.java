b43014b563582778284a1411caae9f99937823e0
Cian Mckenna <cianmckenna@Cians-MacBook-Air.local>
2023-03-07 12:07:11 +0000

    End of Class

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -39,11 +39,11 @@ public class Main
         processing.core.PApplet.runSketch( a, new Audio2());
     }
 
-	public static void audio3()
+	/*public static void audio3()
 	{
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new Audio3());
-    }
+    }*/
 
 	public static void life()
 	{
